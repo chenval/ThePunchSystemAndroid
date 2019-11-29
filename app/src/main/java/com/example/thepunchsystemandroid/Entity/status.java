766d6fc0c7812234a5b8f9f11fe1052bddad8c70
chenval <402414521@qq.com>
2019-11-29 22:11:15 +0800

    优化查询

--- app/src/main/java/com/example/thepunchsystemandroid/Entity/status.java
+++ app/src/main/java/com/example/thepunchsystemandroid/Entity/status.java
@@ -3,11 +3,4 @@ package com.example.thepunchsystemandroid.Entity;
 public class status {
     private String status;
 
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
